4dcf4825512a89b3817141ea829138ad12e771c3
Corey Crosby <ccc_13@live.ca>
2019-05-21 13:36:05 -0400

    completed exercise

---
+++ exercise.py
@@ -0,0 +1,27 @@
+event_rooms = { 'data': { 'rooms': 
+    [ { 'id': 1, 'room_number': "201", 'capacity': 50}, { 'id': 2, 'room_number': "301", 'capacity': 200 }, { 'id': 3, 'room_number': "1B", 'capacity': 100}
+    ],
+    'events': [
+      { 'id': 1, 'room_id': 2, 'start_time': 18, 'end_time': 20, 'attendees': 75 },
+      { 'id': 2, 'room_id': 1, 'start_time': 10, 'end_time': 18, 'attendees': 25 },
+      { 'id': 3, 'room_id': 2, 'start_time': 10, 'end_time': 18, 'attendees': 20 },
+      { 'id': 4, 'room_id': 3, 'start_time': 18, 'end_time': 21, 'attendees': 56 },
+    ]
+  }
+}
+
+#Retrieve the capacity of room 201 and store it in a variable.
+room_201 = event_rooms["data"]["rooms"][0]["capacity"]
+print(room_201)
+
+#Find all the events taking place in room 201. Iterate through them and print "ok" if the number of planned attendees will fit in the room.
+def check_capacity():
+
+    for event in event_rooms["data"]["events"]:
+        if event["attendees"] <= room_201:
+            print(f"Event{event['id']}: ok")
+        else:
+            print(f"Event{event['id']}: will not fit")
+
+check_capacity()
+
